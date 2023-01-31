6fb3f1840bf15e4897a58ad404765b24d572ff47
Krystina Witt <krystinawitt@gmail.com>
2023-01-31 15:11:12 +0100

    change to ejs and add partials and layouts

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
+//https://github.com/Doomfang3/express-basics-jan23
+
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/public'))
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(expressLayouts)
+
 app.get('/', (req, res) => {
   console.log(req.url)
-  res.sendFile(__dirname + '/views/index.html')
+  //res.sendFile(__dirname + '/views/index.html')
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
   console.log(req.url)
-  res.sendFile(__dirname + '/views/about.html')
+  res.render('about')
 })
 
 app.get('/works', (req, res) => {
     console.log(req.url)
-    res.sendFile(__dirname + '/views/works.html')
+    res.render('works')
   })
 
   app.get('/gallery', (req, res) => {
     console.log(req.url)
-    res.sendFile(__dirname + '/views/gallery.html')
+    res.render('gallery')
   })
 
 app.listen(port, () => {
